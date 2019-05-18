45b0576e94844df95029f7c708214d7d3e20bf34
Giovannni Bologni <giovannibologni@gmail.com>
2019-05-18 18:09:19 +0200

    Bug fix

--- reading_ultrasonic/reading_ultrasonic.ino
+++ reading_ultrasonic/reading_ultrasonic.ino
@@ -1,13 +1,13 @@
 #define trigPin 3
 #define echoPin 2
-#define MAX_READINGS 40
+#define MAX_READINGS 30
 
 double duration, distance, prev_distance, avg_distance;
 double differences[MAX_READINGS];
 double sum;
 short count;
 
-//float alpha; // factor to tune
+float alpha; // factor to tune: 0 = maximum averaging. 1 = no average.
 
 
 void setup() {
@@ -15,13 +15,16 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
-//  alpha = 0.2;
+  alpha = 0.8;
   count = 0;
   sum = 0;
   prev_distance = 0;
 }
 
 void loop() {
+  // Measure distance from sensor by sending an ultrasonic signal
+  // and measuring how much time is needed for the signal to be
+  // reflected back.
   digitalWrite(trigPin, LOW);  // Added this line
   delayMicroseconds(2); // Added this line
   digitalWrite(trigPin, HIGH);
@@ -30,9 +33,8 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
 
-  Serial.print(distance);
-  Serial.print(" ");
 //  avg_distance = alpha * distance + (1-alpha) * avg_distance;
+//  distance = avg_distance;
 //  Serial.println(avg_distance);
 
   // Measure difference between consecutive readings
@@ -45,7 +47,10 @@ void loop() {
   count += 1;
 
   // Print the average difference of the last "count" readings
-  if (count == MAX_READINGS){
+  if (count % 10 == 0){
+    Serial.print("Difference over ");
+    Serial.print(count);
+    Serial.print(" samples: ");
     Serial.println(sum/(count-1));
   }
 
@@ -56,5 +61,5 @@ void loop() {
   }
 
   // Wait before reading the distance again
-  delay(100);
+  delay(70);
 }
